3815def00b4c5229e39996e0b3da2b8ca2a86790
Rahul Shah <rahulshah7@outlook.com>
2020-01-08 03:12:44 +0000

    Lint files and amend as suggested

--- controllers/entries.js
+++ controllers/entries.js
@@ -3,11 +3,11 @@ const entries = [
   { id: 2, gratitude: "I'm grateful for working on codings." }
 ];
 
-module.exports.getAll = (req, res, next) => {
+module.exports.getAll = (req, res) => {
   res.status(200).json(entries);
 };
 
-module.exports.create = (req, res, next) => {
+module.exports.create = (req, res) => {
   const { gratitude } = req.body;
   entries.push({ id: Math.random().toString(), gratitude });
   res.status(201).json(entries[entries.length - 1]);
