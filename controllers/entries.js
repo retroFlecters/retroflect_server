4727d2646c1a364525b65f6dd71e11914da3c1f5
Rahul Shah <rahulshah7@outlook.com>
2020-01-08 21:58:03 +0000

    Set 'toJSON' to clean up of returned MongoDB documents

--- controllers/entries.js
+++ controllers/entries.js
@@ -2,11 +2,11 @@ const Entry = require("../models/entry");
 
 module.exports.getAll = async (req, res) => {
   const entries = await Entry.find();
-  res.status(200).json(entries);
+  res.status(200).json(entries.map(entry => entry.toJSON()));
 };
 
 module.exports.create = async (req, res) => {
   const { entryDate, diary } = req.body;
   const entry = await Entry.create({ entryDate, diary });
-  res.status(201).json(entry);
+  res.status(201).json(entry.toJSON());
 };
--- models/entry.js
+++ models/entry.js
@@ -10,4 +10,14 @@ const EntrySchema = new Schema(
   { timestamps: true }
 );
 
+EntrySchema.set("toJSON", {
+  // remove the __v property from the json representation
+  versionKey: false,
+  // replace the "_id" key with "id" and convert value to string
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+  }
+});
+
 module.exports = mongoose.model("Entry", EntrySchema);
