3e0c32ea82c081f21508bca85fb237a32d0e30e8
Rahul Shah <rahulshah7@outlook.com>
2020-01-08 15:22:43 +0000

    Add created, modified timestamps

--- models/entry.js
+++ models/entry.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const EntrySchema = new Schema({
-  entryDate: Date,
-  diary: String
-});
+const EntrySchema = new Schema(
+  {
+    entryDate: Date,
+    diary: String
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Entry", EntrySchema);
