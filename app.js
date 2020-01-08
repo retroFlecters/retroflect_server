f9c59c4c0d63c97ae0daef2df3c793f15286993e
Rahul Shah <rahulshah7@outlook.com>
2020-01-08 12:17:22 +0000

    Set up MongoDB connection

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const entriesRouter = require("./routes/entriesRouter");
+
+// Set up mongoose connection
+const mongoDB = "mongodb://127.0.0.1:27017/retro_development";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
 
