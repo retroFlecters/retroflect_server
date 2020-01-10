2c14b9c055e19d8468312af311ff7ac4ee6a2f28
Rahul Shah <rahulshah7@outlook.com>
2020-01-10 00:52:10 +0000

    Save password hash on sign up; compare on sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 module.exports.signup = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  const passwordHash = password;
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
   const user = await User.create({ firstName, lastName, email, passwordHash });
   res.status(201).json(user.toJSON());
 };
@@ -11,9 +13,12 @@ module.exports.signin = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
-  const passwordHash = password;
+  let isPasswordCorrect = false;
+  if (user) {
+    isPasswordCorrect = await bcrypt.compare(password, user.passwordHash);
+  }
 
-  if (user.passwordHash === passwordHash) {
+  if (isPasswordCorrect) {
     res.status(200).json(user.toJSON());
   } else {
     res.status(404).json("Invalid email or password.");
